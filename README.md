f00015e3927ef7cf79aa731c3a5178ae77055e68
DarioValeriano <dvaleriano@fi.uba.ar>
2024-04-15 03:17:35 -0300

    Version 1.0

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Portfolio
 
-holaaaa
-que raro
+
+nuevo
---
+++ public/js/eventos.js
@@ -0,0 +1,36 @@
+const menu = document.querySelector('.menu-hamburguesa')
+const lista = document.querySelector('.lista-nav')
+const navbar = document.querySelector('header')
+
+menu.addEventListener('click', () => {
+    if(window.scrollY == 0){
+        if(lista.classList.contains('invisible')){
+            lista.classList.remove('invisible')
+            navbar.classList.add('blured')
+            navbar.classList.add('redimension')
+        } else {
+            lista.classList.add('invisible')
+            navbar.classList.remove('blured')
+            navbar.classList.remove('redimension')
+        }
+    }else{
+        if(lista.classList.contains('invisible')){
+            lista.classList.remove('invisible')
+            navbar.classList.add('redimension')
+        } else{
+            lista.classList.add('invisible')
+            navbar.classList.remove('redimension')
+        }
+    }
+})
+
+window.addEventListener("scroll", () =>{
+    if(lista.classList.contains('invisible')){
+        navbar.classList.toggle("blured", window.scrollY > 0)
+    }
+    
+})
+
+//
+
+
